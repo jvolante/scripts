909a71f3ddbe73cdf4140d374ef51cd4c443d00c
Jackson Volante <jvolante96@gmail.com>
2026-01-02 16:42:44 -0500

    refactor: move overlay packages to jvscripts namespace

--- flake.nix
+++ flake.nix
@@ -20,21 +20,21 @@
       in
       {
         packages = {
-          backup-gpg-keys = pkgs.backup-gpg-keys;
-          build-flake-packages = pkgs.build-flake-packages;
-          find-git-repos = pkgs.find-git-repos;
-          get-forge-link = pkgs.get-forge-link;
-          git-is-merged = pkgs.git-is-merged;
-          jxl-converter = pkgs.jxl-converter;
-          list-cpp-includes = pkgs.list-cpp-includes;
-          mkgitremote = pkgs.mkgitremote;
-          mklicense = pkgs.mklicense;
-          omnimv = pkgs.omnimv;
-          plantpreview = pkgs.plantpreview;
-          replace-in-files = pkgs.replace-in-files;
-          restore-gpg-keys = pkgs.restore-gpg-keys;
-          rip-and-eject = pkgs.rip-and-eject;
-          yamldiff = pkgs.yamldiff;
+          backup-gpg-keys = pkgs.jvscripts.backup-gpg-keys;
+          build-flake-packages = pkgs.jvscripts.build-flake-packages;
+          find-git-repos = pkgs.jvscripts.find-git-repos;
+          get-forge-link = pkgs.jvscripts.get-forge-link;
+          git-is-merged = pkgs.jvscripts.git-is-merged;
+          jxl-converter = pkgs.jvscripts.jxl-converter;
+          list-cpp-includes = pkgs.jvscripts.list-cpp-includes;
+          mkgitremote = pkgs.jvscripts.mkgitremote;
+          mklicense = pkgs.jvscripts.mklicense;
+          omnimv = pkgs.jvscripts.omnimv;
+          plantpreview = pkgs.jvscripts.plantpreview;
+          replace-in-files = pkgs.jvscripts.replace-in-files;
+          restore-gpg-keys = pkgs.jvscripts.restore-gpg-keys;
+          rip-and-eject = pkgs.jvscripts.rip-and-eject;
+          yamldiff = pkgs.jvscripts.yamldiff;
         };
 
         overlays = { default = overlay; };
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,17 +1,19 @@
 { final, prev, self }: {
-  backup-gpg-keys = final.callPackage ./pkgs/backup-gpg-keys { inherit self; };
-  build-flake-packages = final.callPackage ./pkgs/build-flake-packages { inherit self; };
-  find-git-repos = final.callPackage ./pkgs/find-git-repos { inherit self; };
-  get-forge-link = final.callPackage ./pkgs/get-forge-link { inherit self; };
-  git-is-merged = final.callPackage ./pkgs/git-is-merged { inherit self; };
-  jxl-converter = final.callPackage ./pkgs/jxl-converter { inherit self; };
-  list-cpp-includes = final.callPackage ./pkgs/list-cpp-includes { inherit self; };
-  mkgitremote = final.callPackage ./pkgs/mkgitremote { inherit self; };
-  mklicense = final.callPackage ./pkgs/mklicense { inherit self; };
-  omnimv = final.callPackage ./pkgs/omnimv { inherit self; };
-  plantpreview = final.callPackage ./pkgs/plantpreview { inherit self; };
-  replace-in-files = final.callPackage ./pkgs/replace-in-files { inherit self; };
-  restore-gpg-keys = final.callPackage ./pkgs/restore-gpg-keys { inherit self; };
-  rip-and-eject = final.callPackage ./pkgs/rip-and-eject { inherit self; };
-  yamldiff = final.callPackage ./pkgs/yamldiff { inherit self; };
+  jvscripts = {
+    backup-gpg-keys = final.callPackage ./pkgs/backup-gpg-keys { inherit self; };
+    build-flake-packages = final.callPackage ./pkgs/build-flake-packages { inherit self; };
+    find-git-repos = final.callPackage ./pkgs/find-git-repos { inherit self; };
+    get-forge-link = final.callPackage ./pkgs/get-forge-link { inherit self; };
+    git-is-merged = final.callPackage ./pkgs/git-is-merged { inherit self; };
+    jxl-converter = final.callPackage ./pkgs/jxl-converter { inherit self; };
+    list-cpp-includes = final.callPackage ./pkgs/list-cpp-includes { inherit self; };
+    mkgitremote = final.callPackage ./pkgs/mkgitremote { inherit self; };
+    mklicense = final.callPackage ./pkgs/mklicense { inherit self; };
+    omnimv = final.callPackage ./pkgs/omnimv { inherit self; };
+    plantpreview = final.callPackage ./pkgs/plantpreview { inherit self; };
+    replace-in-files = final.callPackage ./pkgs/replace-in-files { inherit self; };
+    restore-gpg-keys = final.callPackage ./pkgs/restore-gpg-keys { inherit self; };
+    rip-and-eject = final.callPackage ./pkgs/rip-and-eject { inherit self; };
+    yamldiff = final.callPackage ./pkgs/yamldiff { inherit self; };
+  };
 }
